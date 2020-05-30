daa7eeafcfb4621a22a39c043dbf6d1cd92f1fa2
Lautaro Ivan Gonzalez <glautaroivan@gmail.com>
2020-05-30 15:40:02 -0300

    configuracion. Si vas a tocar las rutas tenes que hacerlo ahi o explota todo

--- src/main/java/soporte/TestIndexador.java
+++ src/main/java/soporte/TestIndexador.java
@@ -47,10 +47,11 @@ public class TestIndexador {
         ModeloVectorial mv = new ModeloVectorial();
         ArrayList<Documento> docs = mv.procesarBusqueda("get", 2);
 
+        /*
         for (Documento next : docs) {
             System.out.println(next.toString());
         }
-        
+        */
      
      
       
